0eaee2527287c9cfffd0c9a49935d2b3a9d12a2b
daseeds <cyril.jean@gmail.com>
2025-05-03 17:04:38 +0200

    change

--- disko.nix
+++ disko.nix
@@ -1,148 +1,39 @@
-{ config, builtins, ... }:
-let
-  diskMain = builtins.head config.zfs-root.bootDevices;
-in
+# USAGE in your configuration.nix.
+# Update devices to match your hardware.
+{
+ imports = [ ./disko-config.nix ];
+ disko.devices.disk.main.device = "/dev/disk/by-id/sdanvme-Vi3000_Internal_PCIe_NVMe_M.2_SSD_256GB_493734484830052";
+# ata-ST14000NM005G-2KG133_ZL2HVF8F
+# ata-ST14000NM005G-2KG133_ZL2HXWBS
+# ata-ST14000NM005G-2KG133_ZL2K6NBN
+# ata-ST14000NM005G-2KG133_ZTM0A0Q0
+
+}
 {
   disko.devices = {
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-id/${diskMain}";
         content = {
           type = "gpt";
           partitions = {
-            efi = {
-              size = "1G";
+            ESP = {
               type = "EF00";
+              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
-                mountpoint = "/boot/efis/${diskMain}-part2";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
-            bpool = {
-              size = "4G";
-              content = {
-                type = "zfs";
-                pool = "bpool";
-              };
-            };
-            rpool = {
-              end = "-1M";
-              content = {
-                type = "zfs";
-                pool = "rpool";
-              };
-            };
-            bios = {
+            root = {
               size = "100%";
-              type = "EF02";
-            };
-          };
-        };
-      };
-    };
-    zpool = {
-      bpool = {
-        type = "zpool";
-        options = {
-          ashift = "12";
-          autotrim = "on";
-          compatibility = "grub2";
-        };
-        rootFsOptions = {
-          acltype = "posixacl";
-          canmount = "off";
-          compression = "lz4";
-          devices = "off";
-          normalization = "formD";
-          relatime = "on";
-          xattr = "sa";
-          "com.sun:auto-snapshot" = "false";
-        };
-        mountpoint = "/boot";
-        datasets = {
-          nixos = {
-            type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          "nixos/root" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/boot";
-          };
-        };
-      };
-      rpool = {
-        type = "zpool";
-        mode = "mirror";
-        options = {
-          ashift = "12";
-          autotrim = "on";
-        };
-        rootFsOptions = {
-          acltype = "posixacl";
-          canmount = "off";
-          compression = "zstd";
-          dnodesize = "auto";
-          normalization = "formD";
-          relatime = "on";
-          xattr = "sa";
-          "com.sun:auto-snapshot" = "false";
-        };
-        mountpoint = "/";
-        datasets = {
-          nixos = {
-            type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          "nixos/var" = {
-            type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          "nixos/empty" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/";
-            postCreateHook = "zfs snapshot rpool/nixos/empty@start";
-          };
-          "nixos/home" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/home";
-          };
-          "nixos/var/log" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/var/log";
-          };
-          "nixos/var/lib" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/var/log";
-          };
-          "nixos/config" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/etc/nixos";
-          };
-          "nixos/persist" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/persist";
-          };
-          "nixos/nix" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/nix";
-          };
-          docker = {
-            type = "zfs_volume";
-            size = "50G";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/var/lib/containers";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
             };
           };
         };
