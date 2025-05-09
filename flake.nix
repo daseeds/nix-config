6d18581f1828cfcfdbbaf7963d5bb88148935476
daseeds <cyril.jean@gmail.com>
2025-05-09 12:40:12 +0200

    qsdf

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     secrets = {
-      url = "git+ssh://git@github.com/daseeds/nix-private.git";
+      url = "git+ssh://git@github.com/daseeds/nix-private.git?shallow=1";
       flake = false;
     };
   };
