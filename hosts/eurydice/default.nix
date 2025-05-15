1e8447da9f3651c50ac24f7ad54991c8f739a957
daseeds <cyril.jean@gmail.com>
2025-05-15 20:49:15 +0200

    sdfsdf

--- hosts/eurydice/default.nix
+++ hosts/eurydice/default.nix
@@ -80,6 +80,17 @@
     ssh-to-age
     inputs.helix.packages."${pkgs.system}".helix
   ];
-
+  
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.daseeds = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDTSDCWQn/+uOELLgIG5eYuQk+t8hRn9NzSYNaLaFBPTs2vwcjlGwbFhA4dCE42wcQWjp0b7tr2O5AUtVw1kh1WGN04VrgVaof8/R8yprmh8eNsfcww8Iw3qfR+/acJp4zVoTuCMqsgu89G8GnJDJ5hjJ5xMYTz1e6CFF2fQCK74pDaS9Nk5a+ClB/EIdI5JN98CocfoPq4RYVKyVW/X3P02ZRm4Fs/GJKiaLkiy8XqqQTxjF1KuV2X3pZPZG2bvB1DRJd+9ZqWSsvzyoEX7w5aouRcGdNNRHKvm1H+EU5pZez5snuhxXV+HW/ofoi8cNpRBJfA1VTcavhmboIFMhVATzBlPYd8pEt06KB3TMqNBqr37gvzH2iwQJIw+J9diSP0sdFPwCbsL4cXEths/rEasvSwBvpmf6+NSZj9d5b0adKYR37e7iTp3EsDK5dCOXPkNX2KuVHZ0sdpgSvf5IJ/e9pHkt/WqS/ooZju1G/Jzym7iG3vVGJ+DA7F7f15CwMdDISMuyhLCzKsLN5w9NijIQbb09SDbuXq4kzy96ZVkxrd3CouHFCSJL72Va3cAx9kO4CcEWJl0ngAbvtgxGjNfE6S3tga2I7iZsY9Ot/HPWP+4lZ2EVGpPm8Os4H5R29cOiwapTeeU2c51NtiskoMvdlrujDl+nFxMjWQ0XE77Q== cyril.jean@gmail.com"
+    ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
   system.stateVersion = "24.11";
 }
