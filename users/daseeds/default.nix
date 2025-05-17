bdc82cd09e0a6ed62c2cff6d32ad23db7e145dc8
daseeds <cyril.jean@gmail.com>
2025-05-17 21:18:16 +0200

    qsd

--- users/daseeds/default.nix
+++ users/daseeds/default.nix
@@ -35,4 +35,14 @@
   };
   programs.zsh.enable = true;
 
+  programs.nixvim = {
+    nixpkgs.pkgs = import <nixpkgs> { };
+
+    enable = true;
+    enableMan = true; # install man pages for nixvim options
+
+    clipboard.register = "unnamedplus"; # use system clipboard instead of internal registers
+
+  }
+
 }
