545042ca048993e3f5ded3ab78dc83b9d56c66f4
daseeds <cyril.jean@gmail.com>
2025-05-17 21:56:55 +0200

    qsdf

--- hosts/common/sops.nix
+++ hosts/common/sops.nix
@@ -10,5 +10,12 @@ in
   sops = {
 
     defaultSopsFile = "${secretspath}/secrets.yaml";
+
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true;
+    };
+
   };
 }
